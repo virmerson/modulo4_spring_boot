51116d44cd65622e70a061a9188ca0716f496871
virmerson <virmerson@hightechcursos.com.br>
2016-08-06 08:38:27 -0400

    Iniciando com JPA

---
+++ src/main/java/br/com/fabricadeprogramador/Modulo4Application.java
@@ -0,0 +1,12 @@
+package br.com.fabricadeprogramador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo4Application.class, args);
+	}
+}
---
+++ src/main/java/br/com/fabricadeprogramador/model/Cliente.java
@@ -0,0 +1,44 @@
+package br.com.fabricadeprogramador.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id @GeneratedValue 
+	private Long id;
+	
+	private String nome;
+	
+	@Column(nullable=false, length=50)
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
